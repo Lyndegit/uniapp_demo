250032b9e17871a93a4a77fbff99a11cb7ca4814
Lyndegit <2061554200@qq.com>
2019-07-13 10:33:50 +0800

    uni_demo

---
+++ pages/chenggong/chenggong.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="main">
+		恭喜您，登录成功啦！！！
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.main{
+	font-size: 99upx;
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,79 @@
+<template>
+	<view class="content uni-flex uni-column">
+		<text class="title">{{title}}</text>
+		<view class="flex-item flex-item-V">
+            <span>用户名:</span> 
+			<input class="uni-input" focus placeholder="请输入用户名" v-model="loginInfoVo.username"/>
+		</view>
+		<view class="flex-item flex-item-V">
+			<span>密码:</span> 
+			<input type="uni-input" focus placeholder="请输入密码"  v-model="loginInfoVo.userpass"/>
+		</view>
+		<view class="flex-item flex-item-V">
+			<button type="primary" class="bt" @tap="fun">获取验证码</button>
+		    <span>
+				{{yanzhen}}
+			</span>
+			<input type="text" v-model="loginInfoVo.useryan"/>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '登录页面',
+				yanzhen:'',
+				 loginInfoVo: { username: '', password: '' ,useryan:''},
+			}
+		},
+		onLoad:function demo(e) {
+			uni.request({
+				url:'',
+				methods:'POST',
+				data:{
+					 username: this.loginInfoVo.username,
+                     password: this.loginInfoVo.password,
+				},
+				success:res=>{
+					console.log(res);
+				},
+				fall:failResponse => {}
+			})
+		
+           
+		},
+		methods: {
+         fun(){
+			 if(1) //此处等待后台挂钩子函数demo
+			 uni.navigateTo({
+			 	url:'../chenggong/chenggong',
+			 })
+		 }
+			 
+		 }
+		}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 50upx;
+		width: 50upx;
+		margin-top: 0upx;
+	}
+
+	.title {
+		font-size: 56upx;
+		color: black;
+	}
+		
+	.bt{
+		width: 50%;
+	}
+</style>
